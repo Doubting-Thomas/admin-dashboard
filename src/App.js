63706ab108fd6c11a63b3a1a804f389ad1506725
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-04-05 12:05:49 -0700

    chore: add basic component files

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="App">Nice dashboard</div>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="dashboard">
+        <Sidebar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
