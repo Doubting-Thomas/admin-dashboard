21c9355681a8e3cbcd55fc987013eb42985aedb8
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-04-12 08:02:37 -0700

    feat: add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
-function App() {
+const App = () => {
   return (
     <div className="dashboard">
+      <h1>Let's get started</h1>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Sidebar />} />
-        </Routes>
+        <Sidebar>
+          <Routes>
+            {/* <Route path="/" element={<Sidebar />} /> */}
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Sidebar>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,78 @@
 import React from "react";
+import css from "./Sidebar.module.css";
+import { MdSpaceDashboard } from "react-icons/md";
+import { AiFillCalendar, AiOutlineTable } from "react-icons/ai";
+import { FaTasks, FaBars } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const Sidebar = ({ children }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggle = () => setIsCollapsed(!isCollapsed);
+
+  const menuItem = [
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      icon: <MdSpaceDashboard />,
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      icon: <AiFillCalendar />,
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      icon: <AiOutlineTable />,
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      icon: <FaTasks />,
+    },
+  ];
+  return (
+    <div className={css.container}>
+      <div
+        style={{ width: isCollapsed ? "150px" : "50px" }}
+        className={css.sidebar}
+      >
+        <div className={css.top}>
+          <h1
+            style={{ display: isCollapsed ? "block" : "none" }}
+            className={css.logo}
+          >
+            Logo
+          </h1>
+          <div
+            style={{ marginLeft: isCollapsed ? "50px" : "0px" }}
+            className={css.bars}
+          >
+            <FaBars onClick={toggle} />
+          </div>
+        </div>
+
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className={css.link}
+            activeClassName={css.active}
+          >
+            <div className={css.icon}>{item.icon}</div>
+            <div
+              style={{ display: isCollapsed ? "block" : "none" }}
+              className={css.text}
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
+      </div>
+      <main>{children}</main>
+    </div>
+  );
 };
 export default Sidebar;
