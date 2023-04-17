25863c79a1f3e975fe89ad715ab6d187294208d5
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-04-17 14:06:56 -0700

    feat: render card components on page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Topbar from "../components/Topbar/Topbar";
+import Card from "../components/Card/Card";
 
 const Dashboard = () => {
   return (
     <div>
       <Topbar />
       <h2>Dashboard</h2>
+      <Card />
     </div>
   );
 };
