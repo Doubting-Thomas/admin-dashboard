db381612b634ca937847b89806067cb010ddd283
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-04-20 12:42:30 -0700

    fix: remove unused code

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -12,9 +12,7 @@ const Stats = () => {
         <div className={css.header}>
           <h3 className={css.title}>Revenue Generated</h3>
 
-          {/* <div className={css.statsIcon}> */}
           <BsFillArrowUpSquareFill size={20} />
-          {/* </div> */}
         </div>
 
         <div className={css.chart}>
