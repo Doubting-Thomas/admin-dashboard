db0a9c5e2e1d78b99c93f1eef820834422dc389f
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-05-01 10:34:01 -0700

    feat: render table component on display

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Topbar from "../components/Topbar/Topbar";
 import Card from "../components/Card/Card";
 import Stats from "../components/Stats/Stats";
+import css from "./Dashboard.module.css";
+// import OrderTable from "../components/TableData/TableData";
+import OrderTable from "../components/TableData/TableData";
 
 const Dashboard = () => {
   return (
@@ -10,6 +13,10 @@ const Dashboard = () => {
       <h2>Dashboard</h2>
       <Card />
       <Stats />
+
+      <div className={css.bottom}>
+        <OrderTable />
+      </div>
     </div>
   );
 };
