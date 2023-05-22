3ebc1beab07377d7d5c3cfa99c915f9b15c86e5c
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-05-22 11:32:46 -0700

    chore: setting up page files

---
+++ src/pages/BoardPage/BoardPage.jsx
@@ -0,0 +1,11 @@
+import Board, {
+  moveCard,
+  moveColumn,
+  removeCard,
+  addCard,
+} from "@asseinfo/react-kanban";
+import "@asseinfo/react-kanban/dist/styles.css";
+import css from "./BoardPage.module.css";
+
+const BoardPage = () => {};
+export default BoardPage;
