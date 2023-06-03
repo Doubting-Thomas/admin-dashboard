4749b263824db457bf9946358937eb26974e05f5
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-06-03 06:24:45 -0700

    add routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import BoardPage from "./pages/BoardPage/BoardPage";
+import Calendar from "./pages/Calendar/Calendar";
+import DataGrid from "./pages/DataGrid/DataGrid";
+//import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const App = () => {
+  //const { pathName } = useLocation();
   return (
-    <div className="dashboard">
+    <div className="container">
+      {/* having the dashboard as the default route */}
+      {/* {pathName === "/" && <Navigate to="/dashboard" />} */}
       <BrowserRouter>
         <Sidebar>
           <Routes>
             {/* <Route path="/" element={<Sidebar />} /> */}
+            <Route path="/" element={<Dashboard />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/boardpage" element={<BoardPage />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/datagrid" element={<DataGrid />} />
           </Routes>
         </Sidebar>
       </BrowserRouter>
+
+      {/* having the dashboard as the default route */}
+      {/* <div className="content">
+        <Outlet />
+      </div> */}
     </div>
   );
 };
