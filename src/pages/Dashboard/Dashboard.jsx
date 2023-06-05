f21b55dd049816e714b8d5b9a22e71c479b8dc2a
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-06-05 04:23:41 -0700

    feat: add dashboard functionalities

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Topbar from "../../components/Topbar/Topbar";
+import Card from "../../components/Card/Card";
+import Stats from "../../components/Stats/Stats";
+import css from "./Dashboard.module.css";
+// import OrderTable from "../components/TableData/TableData";
+import OrderTable from "../../components/TableData/TableData";
+import PieChartData from "../../components/PieChart/PieChart";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Topbar />
+      <h2>Dashboard</h2>
+      <Card />
+      <Stats />
+
+      <div className={css.dashboard__bottom}>
+        <OrderTable />
+
+        <div className={css.dashboard__pie}>
+          <PieChartData />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Dashboard;
