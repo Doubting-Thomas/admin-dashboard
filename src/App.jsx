ae9c4861c7494c3e049fd077d028e4a362e354c6
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-06-10 06:13:46 -0700

    feat: add routes for chart components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Calendar from "./pages/Calendar/Calendar";
 import UserData from "./pages/UserData/UserData";
+import PieChartData from "./components/PieChart/PieChart";
+import StatsChart from "./components/StatsChart/StatsChart";
+import BarChart from "./components/BarChart/BarChart";
 
 const App = () => {
   return (
@@ -15,6 +18,9 @@ const App = () => {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/userdata" element={<UserData />} />
+            <Route path="/piechart" element={<PieChartData />} />
+            <Route path="/statschart" element={<StatsChart />} />
+            <Route path="/barchart" element={<BarChart />} />
           </Routes>
         </Sidebar>
       </BrowserRouter>
